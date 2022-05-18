59aa79820111a3d3dfbb5acb4feb0ce1c826c7d8
Tasenko03 <93547677+Tasenko03@users.noreply.github.com>
2022-05-18 20:31:54 +0300

    теперь словари должны правильно работать

--- game.py
+++ game.py
@@ -1,18 +1,10 @@
 import random
-
-
 while True:
-    def language(answer_language: str) -> str:
-        while True:
-            if answer_language == "1" or answer_language == "english":
-                return "1"
-            elif answer_language == "2" or answer_language == "русский":
-                return "2"
-            else:
-                print("Попробуйте еще раз/Try again")
-                answer_language = input().lower().strip()
-
-
+    def language(answer_language: int) -> int:
+        if answer_language == 1:
+            return 1
+        elif answer_language == 2:
+            return 2
     def rules(answer: str) -> None:
         while True:
             if answer == "да":
@@ -23,9 +15,11 @@ while True:
                 )
                 break
             elif answer == "yes":
-                print("""\nYou need to guess the word specified in the riddle. You have to choose the letter that is 
-        in the word. If you are wrong, then a gallows is drawn. Finish the game before the gallows are drawn! 
-        The game has started! """)
+                print(
+                    """\nYou need to guess the word specified in the riddle. You have to choose the letter that is 
+in the word. If you are wrong, then a gallows is drawn. Finish the game before the gallows are drawn! 
+The game has started! """
+                )
                 break
             elif answer == "нет":
                 print("\nИгра началась!")
@@ -34,33 +28,34 @@ while True:
                 print("\nThe game has started!")
                 break
             else:
-                print("\nВведенные данные некорректны!/The data entered is incorrect!")
-                answer = input().lower().strip()
+                if answer_language == 1:
+                    print("\nThe data entered is incorrect!")
+                    answer = input().lower().strip()
+                else:
+                    print("\nВведенные данные некорректны!")
+                    answer = input().lower().strip()
 
-
-    def level(level_answer: str) -> dict:
+    def level(level_answer: int) -> dict:
         while True:
-            if level_answer == "1" and language(level_answer) == "1":
+            if level_answer == 1 and language(answer_language) == 1:
                 words = English_dictionary_easy
                 print("You have chosen an easy level!")
                 return words
-            elif level_answer == "2" and language(level_answer) == "1":
+            elif level_answer == 2 and language(answer_language) == 1:
                 words = English_dictionary_hard
                 print("You have chosen a complex level!")
                 return words
-            elif level_answer == "1" and language(level_answer) == "2":
+            elif level_answer == 1 and language(answer_language) == 2:
                 words = Russian_dictionary_easy
                 print("Вы выбрали легкий уровень сложности!")
                 return words
-            elif level_answer == "2" and language(level_answer) == "2":
+            elif level_answer == 2 and language(answer_language) == 2:
                 words = Russian_dictionary_hard
                 print("Вы выбрали сложный уровень!")
                 return words
             else:
                 print("\nВведенные данные некорректны!/The data entered is incorrect!")
-                level_answer = input().strip()
-
-
+                level_answer = int(input().strip())
     picture_of_man = (
         """
         --------
@@ -82,6 +77,16 @@ while True:
         """,
         """
         --------
+        |      |
+        |      0
+        |     
+        |     
+        |
+        ----------
+        """,
+        """
+        --------
+        |      |
         |      0
         |      X
         |      
@@ -126,47 +131,120 @@ while True:
     )
     max_wrong = len(picture_of_man) - 1
     Russian_dictionary_easy = {
-        "мебель": ("диван", "стол", "табурет", "шкаф", "вешалка", "кресло", "кровать", "стул",
-                   "этажерка"),
-        "животные": ("собака", "кошка", "свинья", "мышь", "жираф", "слон", "тигр", "лев",
-                     "волк"),
-        "одежда": ("кофта", "брюки", "платье", "рубашка", "юбка", "носки", "свитер", "пальто",
-                   "куртка"),
-        "фрукты": ("банан", "апельсин", "яблоко", "ананас", "мандарин", "киви", "персик",
-                   "абрикос", "груша")}
-
-    English_dictionary_easy = {
-        "furniture": ("sofa", "table", "stool", "wardrobe", "hanger", "armchair", "bed", "chair",
-                      "bookcase"),
-        "animals": ("dog", "cat", "pig", "mouse", "giraffe", "elephant", "tiger", "lion",
-                    "wolf"),
-        "clothes": ("jacket", "trousers", "dress", "shirt", "skirt", "socks", "sweater", "coat",
-                    "jacket"),
-        "fruits": ("banana", "orange", "apple", "pineapple", "tangerine", "kiwi", "peach",
-                   "apricot", "pear")}
-
+        "мебель": (
+            "диван",
+            "стол",
+            "табурет",
+            "шкаф",
+            "вешалка",
+            "кресло",
+            "кровать",
+            "стул",
+        ),
+        "животные": (
+            "собака",
+            "кошка",
+            "свинья",
+            "мышь",
+            "жираф",
+            "слон",
+            "тигр",
+            "лев",
+            "волк",
+        ),
+        "одежда": (
+            "кофта",
+            "брюки",
+            "платье",
+            "рубашка",
+            "юбка",
+            "носки",
+            "свитер",
+            "пальто",
+            "куртка",
+        ),
+        "фрукты": (
+            "банан",
+            "апельсин",
+            "яблоко",
+            "ананас",
+            "мандарин",
+            "персик",
+            "абрикос",
+            "груша",
+        ),
+    }
+    English_dictionary_easy = {"furniture": ("sofa", "table", "stool", "wardrobe", "hanger", "armchair", "bed", "chair",
+                                             "bookcase"),
+                               "animals": ("dog", "cat", "pig", "mouse", "giraffe", "elephant", "tiger", "lion",
+                                           "wolf"),
+                               "clothes": ("jacket", "trousers", "dress", "shirt", "skirt", "socks", "sweater", "coat",
+                                           "jacket"),
+                               "fruits": ("banana", "orange", "apple", "pineapple", "tangerine", "kiwi", "peach",
+                                          "apricot", "pear")}
     Russian_dictionary_hard = {
-        "физика": ("амплитуда", "вакуум", "вольтметр", "дифракция", "изолятор", "интерференция",
-                   "конвекция", "конденсация", "радиоактивность")}
-
-    English_dictionary_hard = {"physics": ("amplitude", "vacuum", "voltmeter", "diffraction", "insulator",
-                                           "interference", "convection", "condensation", "radioactivity")}
-
+        "физика": (
+            "амплитуда",
+            "вакуум",
+            "вольтметр",
+            "дифракция",
+            "изолятор",
+            "интерференция",
+            "конвекция",
+            "конденсация",
+            "радиоактивность",
+        )
+    }
+    English_dictionary_hard = {
+        "physics": (
+            "amplitude",
+            "vacuum",
+            "voltmeter",
+            "diffraction",
+            "insulator",
+            "interference",
+            "convection",
+            "condensation",
+            "radioactivity",
+        )
+    }
     print("Your language/Ваш язык: 1) English 2) Русский")
-    answer_language = input().lower().strip()
-    language(answer_language)
-    if language(answer_language) == "2":
-        print("Хотите ли Вы ознакомиться с правилами? Да/Нет")
-    else:
-        print("Do you want to read the rules? Yes/No")
+    while True:
+        try:
+            answer_language = int(
+                input("Write the number of the language/Напишите цифру языка: ").strip()
+            )
+            language(answer_language)
+        except ValueError:
+            print("Попробуйте еще раз/Try again")
+        else:
+            if language(answer_language) == 1:
+                print("Do you want to read the rules? Yes/No")
+                break
+            else:
+                print("Хотите ли Вы ознакомиться с правилами? Да/Нет")
+                break
     answer = input().lower().strip()
     rules(answer)
-    if language(answer_language) == "2":
-        print("\nВыберите уровень сложности!\nДля легкого введите 1, для сложного введите 2: ")
-    else:
-        print("\nSelect a difficulty level!\nEnter 1 for Easy level, enter 2 for Complex level: ")
-
-    level_answer = input().strip()
+    while True:
+        try:
+            if language(answer_language) == 2:
+                print(
+                    "\nВыберите уровень сложности!\nДля легкого введите 1, для сложного введите 2: "
+                )
+                level_answer = int(input().strip())
+                break
+            else:
+                print(
+                    "\nSelect a difficulty level!\nEnter 1 for Easy level, enter 2 for Complex level: "
+                )
+                level_answer = int(input().strip())
+                break
+        except ValueError:
+            if language(answer_language) == 2:
+                print("Неверный ввод!")
+            else:
+                print("Wrong input!")
     words1 = level(level_answer)
     topic = random.choice(list(words1.keys()))
     word = random.choice(list(words1[topic]))
@@ -174,21 +252,23 @@ while True:
     wrong = 0
     used_letter = []
     while wrong < max_wrong and length_of_word != word:
-        if language(answer_language) == "2":
+        if language(answer_language) == 2:
             print("\nТема:\n", topic)
         else:
             print("\nTopic:\n", topic)
         print(f"{picture_of_man[wrong]}\n{used_letter}\n{length_of_word}\n")
-        if language(answer_language) == "2":
+        if language(answer_language) == 2:
             print("Введите букву: ")
         else:
             print("Enter a letter: ")
         guess = input().lower()
         while guess in used_letter:
-            if language(answer_language) == "2":
+            if language(answer_language) == 2:
                 print(f"Вы уже вводили букву {guess}. Введите другую букву:")
             else:
-                print(f"You have already entered the letter {guess}. Please enter a different letter:")
+                print(
+                    f"You have already entered the letter {guess}. Please enter a different letter:"
+                )
             guess = input().lower()
         used_letter.append(guess)
         if guess in word:
@@ -202,34 +282,35 @@ while True:
         else:
             wrong += 1
     if wrong == max_wrong:
-        if language(answer_language) == "2":
+        if language(answer_language) == 2:
             print("Вас повесили")
         else:
             print("You've died")
         print(picture_of_man[wrong])
     else:
-        if language(answer_language) == "2":
+        if language(answer_language) == 2:
             print("У тебя получилось!")
         else:
             print("You've won!")
     print(f"\n{used_letter}\n{word}")
     while True:
-        if language(answer_language) == "2":
-            print('Еще раз? (да/нет): ')
+        if language(answer_language) == 2:
+            print("Еще раз? (да/нет): ")
         else:
-            print('Again? (yes/no): ')
+            print("Again? (yes/no): ")
         answer = input().lower().strip()
         if answer in ("да", "нет", "yes", "no"):
             break
-        if language(answer_language) == "2":
-            print('Неверный ввод')
+        if language(answer_language) == 2:
+            print("Неверный ввод")
         else:
-            print('Incorrect type')
-    if answer == 'да' or answer == 'yes':
+            print("Incorrect type")
+    if answer == "да" or answer == "yes":
         continue
     else:
-        if language(answer_language) == "2":
-            print('Спасибо за игру!')
+        if language(answer_language) == 2:
+            print("Спасибо за игру!")
         else:
-            print('Thank you for playing!')
+            print("Thank you for playing!")
         break
+        
