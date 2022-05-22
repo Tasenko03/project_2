5d504d9094bb04be899f6994c14b752b032f1d68
Tasenko03 <93547677+Tasenko03@users.noreply.github.com>
2022-05-22 23:16:51 +0300

    можно вводить 2 слова

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import dictionaries
 
 while True:
 
-    def symbol_corrector(input_u) -> str:
+    def symbol_corrector(input_u: str) -> str:
         while not input_u.isalpha():
             if answer_language == "1":
                 print(
@@ -20,55 +20,63 @@ while True:
         else:
             return input_u
 
-    def rules(answer: str) -> None:
+    def rules(a: str) -> None:
         while True:
-            if answer == "да":
+            if a == "да":
                 print(
                     """\nВам нужно угадать слово, указанное в загадке. Вы должны выбрать букву, которая есть в слове. 
 Если вы ошибаетесь, то рисуется висельник. Завершите игру до того, как нарисуется висельник!"""
                 )
                 break
-            elif answer == "yes":
+            elif a == "yes":
                 print(
                     """\nYou need to guess the word specified in the riddle. You have to choose the letter that is 
 in the word. If you are wrong, then a gallows is drawn. Finish the game before the gallows are drawn! """
                 )
                 break
-            elif answer == "нет":
-                pass
-                break
-            elif answer == "no":
+            elif a == "нет" or a == "no":
                 pass
                 break
             else:
                 if answer_language == "1":
                     print("\nThe data entered is incorrect!")
-                    answer = input().lower().strip()
+                    a = input().lower().strip()
                 else:
                     print("\nВведенные данные некорректны!")
-                    answer = input().lower().strip()
+                    a = input().lower().strip()
 
-    def level(level_answer: str) -> dict:
+    def level(level_a: str) -> dict:
         while True:
-            if level_answer == "1" and answer_language == "1":
+            if level_a == "1" and answer_language == "1":
                 words = dictionaries.English_dictionary_easy
                 print("You have chosen an easy level!")
                 return words
-            elif level_answer == "2" and answer_language == "1":
+            elif level_a == "2" and answer_language == "1":
                 words = dictionaries.English_dictionary_hard
                 print("You have chosen a complex level!")
                 return words
-            elif level_answer == "1" and answer_language == "2":
+            elif level_a == "1" and answer_language == "2":
                 words = dictionaries.Russian_dictionary_easy
                 print("Вы выбрали легкий уровень сложности!")
                 return words
-            elif level_answer == "2" and answer_language == "2":
+            elif level_a == "2" and answer_language == "2":
                 words = dictionaries.Russian_dictionary_hard
                 print("Вы выбрали сложный уровень!")
                 return words
             else:
                 print("\nВведенные данные некорректны!/The data entered is incorrect!")
-                level_answer = input().strip().lower()
+                level_a = input().strip().lower()
+
+    def repeat(c_guess: str, a: list) -> str and list:
+        while c_guess in a:
+            if answer_language == "2":
+                print(f"\nВы уже вводили {c_guess}.")
+            else:
+                print(f"\nYou have already entered {c_guess}.")
+            guess_new = input().lower().strip()
+            c_guess = symbol_corrector(guess_new)
+        a.append(c_guess)
+        return c_guess, a
 
     with open("hangmanpic.txt", "r") as f:
         picture = f.read()
@@ -77,7 +85,7 @@ in the word. If you are wrong, then a gallows is drawn. Finish the game before t
     max_wrong = len(picture_of_man) - 1
     topic = ""
     word = ""
-    print("Your language/Ваш язык: 1) English 2) Русский")
+    print("\nYour language/Ваш язык: 1) English 2) Русский")
     answer_language = (
         input("Write the number of the language/Напишите цифру языка: ").strip().lower()
     )
@@ -106,6 +114,7 @@ in the word. If you are wrong, then a gallows is drawn. Finish the game before t
             "\nChoose the game mode: for two players or for one player.\nEnter the number 1 if for one "
             "player.Enter the number 2 if for two players."
         )
+
     answer_game_mode = input().strip().lower()
     while True:
         if answer_game_mode == "2":
@@ -145,40 +154,46 @@ in the word. If you are wrong, then a gallows is drawn. Finish the game before t
     length_of_word = "_" * len(word)
     wrong = 0
     used_letter = []
+    used_words = []
     while wrong < max_wrong and length_of_word != word:
         if answer_language == "2" and answer_game_mode == "1":
             print("\nТема:\n", topic)
         elif answer_language == "1" and answer_game_mode == "1":
             print("\nTopic:\n", topic)
-        print(f"{picture_of_man[wrong]}\n{used_letter}\n{length_of_word}\n")
+        print(
+            f"{picture_of_man[wrong]}\n{used_letter}\n{used_words}\n{length_of_word}\n"
+        )
         if answer_language == "2":
-            print("Введите букву: ")
+            print("Введите букву или все слово: ")
         else:
-            print("Enter a letter: ")
+            print("Enter a letter or a whole word: ")
         guess = input().lower().strip()
         corrected_guess = symbol_corrector(guess)
-        while corrected_guess in used_letter:
-            if answer_language == "2":
-                print(
-                    f"\nВы уже вводили букву {corrected_guess}. Введите другую букву:"
-                )
+
+        if len(corrected_guess) > 1:
+            repeat(corrected_guess, used_words)
+            if corrected_guess == word:
+                break
             else:
-                print(
-                    f"\nYou have already entered the letter {corrected_guess}. Please enter a different letter:"
-                )
-            guess = input().lower().strip()
-            corrected_guess = symbol_corrector(guess)
-        used_letter.append(corrected_guess)
-        if corrected_guess in word:
-            new = ""
-            for i in range(len(word)):
-                if corrected_guess == word[i]:
-                    new += word[i]
+                wrong += 1
+                if answer_language == "2":
+                    print("\nНеверно")
                 else:
-                    new += length_of_word[i]
-            length_of_word = new
+                    print("\nIncorrectly")
+
         else:
-            wrong += 1
+            repeat(corrected_guess, used_letter)
+            if corrected_guess in word:
+                new = ""
+                for i in range(len(word)):
+                    if corrected_guess == word[i]:
+                        new += word[i]
+                    else:
+                        new += length_of_word[i]
+                length_of_word = new
+            else:
+                wrong += 1
+
     if wrong == max_wrong:
         if answer_language == "2":
             print("\nВас повесили\n(╮°-°)╮┳━━┳ ( ╯°□°)╯ ┻━━┻")
@@ -192,7 +207,7 @@ in the word. If you are wrong, then a gallows is drawn. Finish the game before t
             print("\nYou've won!\n＼(￣▽￣)／")
     print(f"\n{used_letter}\n{word.upper()}")
 
-    while True:  # функция restart
+    while True:  # restart
         if answer_language == "2":
             print("\nЕще раз? (да/нет): ")
         else:
